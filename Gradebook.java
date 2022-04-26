d8a1855c67b2cc26c9dc1e4900b5e349c4cecc4b
mlopez122 <mlopez10@terpmail.umd.edu>
2022-04-25 23:08:45 -0400

    initial work for setup.java and Gradebook.java -- contains pseudocode in some bits

--- Gradebook.java
+++ Gradebook.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 //import ...
 
 /**
@@ -8,16 +13,49 @@
  */
 public class Gradebook {
 
+    String bookName;
+    ArrayList<String> students;
+    /* each assignment has a string containing "name, max-points, weight"; pick these fields up by deliminating on 
+     * commas. the hashMap is of string -> integer. where string is name of student and integer is their
+     * grade. 
+    */
+    LinkedHashMap<String,HashMap<String, Integer>> assignments; 
+    //keyType is TBD
+    keyType key; //-> initializes then after requested encrypted and must match to decrypt end-file
+  
   /* Read a Gradebook from a file */
   public Gradebook(String filename) {
     //create a gradebook object
+    bookName = filename;
+    /*make sure to input the name as "Last, First" -> then check for duplicate names
+    //deliminate based on commas
+    */
+    key = SecureHashRandomThing();
+    students = new ArrayList<String>();
+    assignments = new LinkedHashMap<String,HashMap<String, Integer>>();
+    //pseudo code
+    File created = new File(bookName);
+    created.pushToOutputStream;
   }
 
   /* Create a new gradebook */
   public Gradebook() {
-
+    bookName = "";
+    key = SecureHashRandomThing();
+    students = new ArrayList<String>();
+    assignments = new LinkedHashMap<String,HashMap<String, Integer>>();
+    //pseudo code
+    File created = new File(bookName);
+    created.pushToOutputStream; //create the file in directory output stream
   }
 
+  /* Returns the keyFirst time, do not call multiple times since the key will be encrypted after first call*/
+  public keyType getPlainKeyOneTime(Gradebook requested) {
+      //pseudo code
+      keyType plainKey = requested.key;
+      requested.key = encrypt(key);
+      return plainKey;
+  }
   /* return the size of the gradebook */
   public int size() {
 
