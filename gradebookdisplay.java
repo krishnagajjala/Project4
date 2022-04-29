3be88eae7c77a94f46f6c7b52857a79b8a563f56
krishnagajjala <66845298+krishnagajjala@users.noreply.github.com>
2022-04-28 21:00:40 -0400

    Update gradebookdisplay.java
    added tests but not main return statements

--- gradebookdisplay.java
+++ gradebookdisplay.java
@@ -1,52 +1,141 @@
+import java.io.File;
+import java.util.Arrays;
+
 //import ...
 
 /**
- * Prints out a gradebook in a few ways
- * Some skeleton functions are included
+ * Prints out a gradebook in a few ways Some skeleton functions are included
  */
 public class gradebookdisplay {
-  static boolean verbose = false;
 
-  private void print_Gradebook(...) {
+	private static boolean file_test(String filename) {
+		String inputName = new String(filename);
+		File seek = new File("./" + inputName);
+		if (seek.exists()) {
+			return false;
+		}
+		return true;
+	}
 
-    for(int i = 0; i < num_assigment; i++) {
-      dump_assignment();
-      System.out.println("----------------\n");
-    }
+	public static void main(String[] args) {
+		
+		// testing input is correct
+		if (args.length < 6) {
+			System.out.println("invalid");
+			System.exit(255);
+		}
+		if (!args[1].equals("-N")) {
+			System.out.println("invalid");
+			System.exit(255);
+		}
+		if (!args[3].equals("-K")) {
+			System.out.println("invalid");
+			System.exit(255);
+		}
+		if (!args[5].equals("-PA") && !args[5].equals("-PS") && !args[5].equals("-PF")) {
+			System.out.println("invalid");
+			System.exit(255);
+		}
+		String requestedName = new String(args[2]);
+		if (file_test(requestedName) != true) {
+			System.out.println("invalid");
+			System.exit(255);
+		}
 
-    return;
-  }
+		
+		
+		System.out.println("\nNumber Of Arguments Passed: %d" + args.length);
+		System.out.println("----Following Are The Command Line Arguments Passed----");
+		for (int counter = 0; counter < args.length; counter++)
+			System.out.println("args[" + counter + "]: " + args[counter]);
+		
+		
+		// Decide what is the setting we are in
+		
+		if (args[5].equals("-PA")) {
+			if (Arrays.asList(args).lastIndexOf("-PS") != -1 || Arrays.asList(args).lastIndexOf("-PF") != -1 
+					|| Arrays.asList(args).lastIndexOf("-FN") != -1 || Arrays.asList(args).lastIndexOf("-LN") != -1) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			int ian = Arrays.asList(args).lastIndexOf("-AN");
+			int  ia = Arrays.asList(args).lastIndexOf("-A");
+			int ig = Arrays.asList(args).lastIndexOf("-G");
+			if (ian == -1) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			if ((ia == -1 && ig == -1) || (ia != -1 && ig != -1)) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			String assignName = args[ian+1];
+			
+			//search for assignment in the gradebook here
+			if(assignName not in gradebook) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			
+			if (ia == -1) {
+				//order by grade here and print output
+				return answer;
+			} else {
+				//order by alphabetical here and print output
+				return answer;
+			}
+			
+		}
+		if (args[5].equals("-PS")) {
+			if (Arrays.asList(args).lastIndexOf("-PF") != -1 || Arrays.asList(args).lastIndexOf("-PA") != -1
+					|| Arrays.asList(args).lastIndexOf("-AN") != -1) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			int ifn = Arrays.asList(args).lastIndexOf("-FN");
+			int  iln = Arrays.asList(args).lastIndexOf("-LN");
+			if ((ifn == -1 && iln == -1)) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			
+			String fName = args[ifn+1];
+			String lName = args[iln+1];
+			
+			
+			Student found = NULL;
+			//search for student in the gradebook here
+			if(fName+lName not in gradebook) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			
+			//print output here
+			return answer;
+		}
+		if (args[5].equals("-PF")) {
+			if (Arrays.asList(args).lastIndexOf("-PS") != -1 || Arrays.asList(args).lastIndexOf("-PA") != -1 
+					|| Arrays.asList(args).lastIndexOf("-FN") != -1 || Arrays.asList(args).lastIndexOf("-LN") != -1 
+					|| Arrays.asList(args).lastIndexOf("-AN") != -1) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			int  ia = Arrays.asList(args).lastIndexOf("-A");
+			int ig = Arrays.asList(args).lastIndexOf("-G");
+			if ((ia == -1 && ig == -1) || (ia != -1 && ig != -1)) {
+				System.out.println("invalid");
+				System.exit(255);
+			}
+			if (ia == -1) {
+				//order by grade here and print output
+				return answer;
+			} else {
+				//order by alphabetical here and print output
+				return answer;
+			}
+			
+		}
+		
 
-  private void print_Assignment(...) {
-
-    return;
-  }
-
-  private void print_Student(...) {
-
-    return;
-  }
-
-  private void print_Final(...){
-
-    return;
-  }
-
-  public static void main(String[] args) {
-    int   opt,len;
-    char  *logpath = NULL;
-
-    //TODO Code this
-    if(args.length==1)
-        System.out.println("\nNo Extra Command Line Argument Passed Other Than Program Name");
-    if(args.length>=2)
-    {
-        System.out.println("\nNumber Of Arguments Passed: %d" + args.length);
-        System.out.println("----Following Are The Command Line Arguments Passed----");
-        for(int counter = 0; counter < args.length; counter++)
-          System.out.println("args[" + counter + "]: " + args[counter]);
-          // Decide what is the setting we are in
-    }
-
-  }
+	}
 }
